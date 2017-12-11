c24cff436b3f595aef860bc6e4568239f02ef4aa
minishrink <mathur_akanksha@hotmail.com>
2017-12-11 16:12:32 +0000

    Add comments to vimsetup

--- vimsetup.sh
+++ vimsetup.sh
@@ -1,7 +1,14 @@
 !#/bin/bash
 
+# install vim to a RHEL distro using yum package manager
 sudo yum install -y vim vim-enhanced
+
+# install vim-plug plugin manager
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# download my .vimrc
 curl -o ~/.vimrc "https://raw.githubusercontent.com/minishrink/dotfiles/master/.vimrc"
+
+# install plugins and set my colourscheme
 vim -c ':w' -c ':so %' -c 'PlugInstall' -c ':colo minimalist' ~/.vimrc
